34a43880062211b3e94d5915d879507418069f04
dhanasekarpadmanabhan <mail2dhanasekarpadmanabhan@gmail.com>
2018-08-03 12:06:23 +0530

    'add'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var requirenpm = require('./requirefile')
 requirenpm.app.get('/', function (req,resp) {
   resp.redirect('/camp')
 })
-requirenpm.app.get('/camp',function (req,resp) {
+requirenpm.app.get('/camp', isLoggedIn, function (req,resp) {
   requirenpm.campground.campground.find({},function (err, data) {
     if(err) {  console.log(err);  }
     else {
@@ -12,10 +12,10 @@ requirenpm.app.get('/camp',function (req,resp) {
     }
   })
 })
-requirenpm.app.get('/camp/new', function (req,resp) {
+requirenpm.app.get('/camp/new', isLoggedIn,  function (req,resp) {
   resp.render('campnew')
 })
-requirenpm.app.post('/camp', function (req, resp) {
+requirenpm.app.post('/camp', isLoggedIn,  function (req, resp) {
   console.log(req.body)
   requirenpm.campground.campground.create({
     name:req.body.camp.title,
@@ -29,7 +29,7 @@ requirenpm.app.post('/camp', function (req, resp) {
   })
   resp.redirect('/camp')
 })
-requirenpm.app.get('/camp/:id/edit', function (req, resp) {
+requirenpm.app.get('/camp/:id/edit', isLoggedIn, function (req, resp) {
   requirenpm.campground.campground.findById(req.params.id, function (err,data) {
     if(err) { console.log(err)
     resp.redirect('/camp/'+ req.params.id)
@@ -40,7 +40,7 @@ requirenpm.app.get('/camp/:id/edit', function (req, resp) {
     }
   })
 })
-requirenpm.app.get('/camp/:id', function (req,resp){
+requirenpm.app.get('/camp/:id',isLoggedIn, function (req,resp){
   requirenpm.campground.campground.findById(req.params.id).populate('posts').exec(function (err,data) {
     if(err) { console.log(err)
       console.log("0");
@@ -52,7 +52,7 @@ requirenpm.app.get('/camp/:id', function (req,resp){
     }
   })
 })
-requirenpm.app.put('/camp/:id', function (req, resp) {
+requirenpm.app.put('/camp/:id',isLoggedIn, function (req, resp) {
   requirenpm.campground.campground.findByIdAndUpdate(req.params.id, req.body.camp, function (err, data){
     if (err) { console.log(err) }
     else {
@@ -61,7 +61,7 @@ requirenpm.app.put('/camp/:id', function (req, resp) {
   })
   resp.redirect('/camp')
 })
-requirenpm.app.delete('/camp/:id', function (req, resp) {
+requirenpm.app.delete('/camp/:id',isLoggedIn, function (req, resp) {
   requirenpm.campground.campground.findByIdAndRemove(req.params.id, function (err, data) {
     if (err) { console.log(err)}
     else{
@@ -70,18 +70,19 @@ requirenpm.app.delete('/camp/:id', function (req, resp) {
     resp.redirect('/camp')
   })
 })
-requirenpm.app.get('/delete/campall', function (req, resp) {
+requirenpm.app.get('/delete/campall',isLoggedIn, function (req, resp) {
   requirenpm.deletedb()
   resp.redirect('/camp')
 })
-requirenpm.app.post('/comment', function (req, resp) {
+requirenpm.app.post('/comment',isLoggedIn, function (req, resp) {
   console.log(req.body)
 console.log(req.body.comment.title);
   requirenpm.campground.comments.create({
     title:req.body.comment.title,
     author:req.body.comment.author
   },function (err, data) {
-    if (err) { console.log(err) }
+    if (err) { console.log(err)
+      resp.redirect('/camp') }
     else {
       requirenpm.campground.campground.findById(req.body.comment.id, function (err, values) {
         if(err) { console.log(err); }
@@ -99,7 +100,7 @@ console.log(req.body.comment.title);
     }
   })
 })
-requirenpm.app.get('/comment/:id', function (req, resp) {
+requirenpm.app.get('/comment/:id',isLoggedIn, function (req, resp) {
   requirenpm.campground.comments.findById(req.params.id, function (err,data) {
     if(err) { console.log(err)
       console.log("0");
@@ -110,7 +111,7 @@ requirenpm.app.get('/comment/:id', function (req, resp) {
     }
   })
 })
-requirenpm.app.put('/comment/:id', function (req, resp) {
+requirenpm.app.put('/comment/:id',isLoggedIn, function (req, resp) {
   requirenpm.campground.comments.findByIdAndUpdate(req.params.id, req.body.comm, function (err, data){
     if (err) { console.log(err) }
     else {
@@ -119,7 +120,7 @@ requirenpm.app.put('/comment/:id', function (req, resp) {
   })
   resp.redirect('/camp')
 })
-requirenpm.app.delete('/comment/:id', function (req, resp) {
+requirenpm.app.delete('/comment/:id', isLoggedIn,function (req, resp) {
   requirenpm.campground.comments.findByIdAndRemove(req.params.id, function (err, data) {
     if (err) { console.log(err)}
     else{
@@ -127,6 +128,41 @@ requirenpm.app.delete('/comment/:id', function (req, resp) {
     }
     resp.redirect('/camp')
   })
+})
+requirenpm.app.get('/login', function (req, resp) {
+  resp.render('login')
+})
+requirenpm.app.get('/signup', function(req,resp){
+  resp.render('register')
+})
+requirenpm.app.post('/register', function(req, resp){
+  requirenpm.User.register(new requirenpm.User({username: req.body.username}), req.body.password, function(err, user){
+    if(err) {
+      console.log(err);
+      return resp.redirect('/signup')
+    }
+    requirenpm.passport.authenticate('local')(req, resp, function() {
+      resp.redirect('/camp')
+    })
+  })
+})
+requirenpm.app.post('/login', requirenpm.passport.authenticate('local',{
+  successRedirect: '/camp',
+  failureRedirect: '/login'
+}), function(req, resp) { })
+requirenpm.app.get('/logout', function (req, resp) {
+  req.logout()
+  resp.redirect('/')
+})
+function isLoggedIn(req, resp, next){
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  resp.redirect('/login')
+}
+requirenpm.app.get('/logout', function (req,resp) {
+  req.logout()
+  resp.redirect('/login')
 })
 requirenpm.app.listen(1234,function (){
   console.log('THE PORT 1234 IS RUNNING THE APPLICATION');
--- requirefile.js
+++ requirefile.js
@@ -1,23 +1,37 @@
-var express = require('express')
-var app = express()
-var mongoose = require('mongoose')
-var bodyParser = require('body-Parser')
-var campground = require('./models/camp')
+var express        = require('express')
+var app            = express()
+var mongoose       = require('mongoose')
+var bodyParser     = require('body-Parser')
+var campground     = require('./models/camp')
 var methodOverride = require('method-override')
-var deletedb = require('./seed')
+var deletedb       = require('./seed')
+var LocalStrategy  = require('passport-local')
+var User           = require('./models/user')
+var passport   = require('passport')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride("_method"))
+app.use(require('express-session')({
+  secret: 'yemp camp secret syncronization',
+  resave: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 mongoose.connect('mongodb://localhost/camp')
 
 
 module.exports = {
-  app:app,
-  mongoose:mongoose,
-  bodyParser:bodyParser,
-  campground:campground,
+  app:        app,
+  mongoose:   mongoose,
+  bodyParser: bodyParser,
+  campground: campground,
   methodOverride:methodOverride,
-  deletedb:deletedb
+  deletedb:   deletedb,
+  User:       User,
+  passport:   passport
 }
